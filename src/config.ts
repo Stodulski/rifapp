580ccf6b90cf723bd8acc21e22031494e75464b4
Stodulski <maxisto043@gmail.com>
2025-09-13 20:02:37 -0300

    Bug

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 
 import raffleRoutes from './modules/raffles/routes.js'
 
 app.use(express.json())
+app.use(cors())
+
+app.use(cors({
+  origin: "http://127.0.0.1:5500",
+  credentials: true
+}))
 
 app.use(raffleRoutes)
 
